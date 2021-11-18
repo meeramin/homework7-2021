f65aba6572aaf73a6424857fb28ef7347ecb7610
meeramin <57604570+meeramin@users.noreply.github.com>
2021-11-17 20:26:57 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,7 +5,38 @@ window.addEventListener("load", function() {
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+ document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	video.play();
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+   video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log("New speed is: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.05;
+	console.log("New speed is: " + video.playbackRate);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if(video.muted){
+		document.querySelector("#mute").innerHTML = "Mute";
+		video.muted = false;
+	}
+	else{
+		document.querySelector("#mute").innerHTML = "Unmute";
+		video.muted = true;
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	var x = document.querySelector("#slider").value;
+	audio.volume = x/100.0;
+});
